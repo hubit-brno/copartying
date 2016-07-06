4364ca600b514b0c3fa974afa3a39174c47a9e05
Roman Damborsky <rdamborsky@gmail.com>
2016-07-06 14:09:35 +0200

    3rd party config file

---
+++ universal/3rdParty.example.js
@@ -0,0 +1,14 @@
+const GMAPS_API_KEY = '*** GMAPS KEY ***';
+const FOURSQUARE_CLIENT_ID = '*** CLIENT ID ***';
+const FOURSQUARE_CLIENT_SECRET = '*** CLIENT SECRET ***';
+const FOURSQUARE_API_V = '20160705';
+
+
+
+// no changes needed below
+
+export const GMAPS_ENDPOINT = 'https://www.google.com/maps/embed/v1/';
+export const GMAPS_AUTH_PARAMS = `key=${GMAPS_API_KEY}`;
+
+export const FOURSQUARE_ENDPOINT = 'https://api.foursquare.com/v2/';
+export const FOURSQUARE_AUTH_PARAMS = `client_id=${FOURSQUARE_CLIENT_ID}&client_secret=${FOURSQUARE_CLIENT_SECRET}&v=${FOURSQUARE_API_V}`;
